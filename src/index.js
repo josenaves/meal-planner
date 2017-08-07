944e00d80c32faa481fbce5d318329ca9f5326a2
José Naves Moura Neto <josenaves@gmail.com>
2017-08-07 10:43:28 -0300

    Add redux store and add recipe test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore } from 'redux'
+import reducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+console.log(store)
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
 registerServiceWorker();
